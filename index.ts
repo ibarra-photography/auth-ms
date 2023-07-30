c9cb5790fe613487157fb88bf101fd057a751838
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-07-30 17:09:18 +0200

    FORMAT -index.ts

--- index.ts
+++ index.ts
@@ -1,17 +1,15 @@
-import express from 'express'
-import cors from 'cors'
-import bodyParser from 'body-parser'
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-app.use("*", (req, res)=> { 
-res.status(404).json({message: 'Not found!'})
-})
+app.use('*', (req, res) => {
+  res.status(404).json({ message: 'Not found!' });
+});
 
-app.listen(4000, () => { 
-console.log('listening on port 4000!')
-})
-
-
+app.listen(4000, () => {
+  console.log('listening on port 4000!');
+});
