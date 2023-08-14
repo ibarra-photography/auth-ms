c39dc745a506844211bd8680e23e22198ab2b415
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-08-14 16:06:31 +0200

    ADD - authentication router

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import { router } from './router/router';
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use('/api/v1/authentication', router);
 
 app.use('*', (req, res) => {
   res.status(404).json({ message: 'Not found!' });
