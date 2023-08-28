395bfad08a989959b0b7ef0d0ee4df47ca9aea06
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-08-28 13:39:20 +0200

    FIX - unused error

--- router/router.ts
+++ router/router.ts
@@ -15,6 +15,7 @@ const validateUserController = (req: Request, res: Response) => {
   try {
     const userToValidate: ValidateUserViewModel = req.body;
     res.status(200).json({ message: 'User validated', isValid: true });
+    console.log(`validating ${userToValidate}`);
   } catch {
     res.status(500).json({ message: 'Error on user validation' });
   }
@@ -23,6 +24,7 @@ const validateUserController = (req: Request, res: Response) => {
 const loginController = (req: Request, res: Response) => {
   try {
     const loginUser: LoginVieModel = req.body;
+    console.log(loginUser);
     res.status(200).json({ token: '' });
   } catch {
     res.status(500).json({ message: 'Error on user login' });
@@ -31,6 +33,7 @@ const loginController = (req: Request, res: Response) => {
 
 const generateInvitationController = (req: Request, res: Response) => {
   const invitationRequest: ValidateUserViewModel = req.body;
+  console.log(invitationRequest);
   // Validate user
   // Check permissions
   // Generate invitation
