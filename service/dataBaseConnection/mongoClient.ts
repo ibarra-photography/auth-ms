abd00162da6acd28e9898e54a272f114b219f3ee
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-08-30 17:39:47 +0200

    FIX - Database connection

--- service/dataBaseConnection/mongoClient.ts
+++ service/dataBaseConnection/mongoClient.ts
@@ -1,10 +1,10 @@
-import mongodb, { MongoClient, MongoClientOptions } from 'mongodb';
+import { MongoClient, MongoClientOptions , Db} from 'mongodb';
 
 const mongoClientOptions: MongoClientOptions = {};
 
-const uri = '';
+const uri = 'mongodb://aitor:aitor1234@localhost:27017/mydb';
 
-export const mongoClient: MongoClient = new mongodb.MongoClient(uri, mongoClientOptions);
+export const mongoClient: MongoClient = new MongoClient(uri, mongoClientOptions);
 
 export const dbName = 'auth-db';
 
@@ -13,6 +13,6 @@ export const invitationsCollection = 'invitations';
 export type Collections = typeof usersCollection | typeof invitationsCollection;
 
 export function getPointer(collection: Collections) {
-  const database: mongodb.Db = mongoClient.db(dbName);
+  const database: Db = mongoClient.db(dbName);
   return database.collection(collection);
 }
