17a243b3b50a2c27af3aac8c5c8c6b34679ac206
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-09-07 09:51:45 +0200

    CLEAN - logs

--- router/controllers/loginController.ts
+++ router/controllers/loginController.ts
@@ -5,12 +5,11 @@ import { findUser } from '../../service/dataBaseConnection/findUser';
 export const loginController = async (req: Request, res: Response) => {
   try {
     const loginUser: LoginViewModel = req.body;
-    console.log(loginUser);
 
     const userViewModel = await findUser(loginUser.username);
     if (userViewModel!.password! !== loginUser.password) throw new Error('Invalid username or password');
 
-    res.status(200).json({ token: '' });
+    res.status(200).json({ token: '', message:"Success!" });
   } catch (error: Error | unknown) {
     console.log(error);
     res.status(500).json({ message: error ? error : 'Error on login' });
