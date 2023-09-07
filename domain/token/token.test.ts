d2dd3d62fca07b51521d32ed4fbdd18e54998772
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-09-07 14:31:30 +0200

    FORMAT

--- domain/token/token.test.ts
+++ domain/token/token.test.ts
@@ -1,31 +1,28 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect } from 'vitest';
 
-import Token from "./token"
-import { Jwt } from 'jsonwebtoken'
+import Token from './token';
+import { Jwt } from 'jsonwebtoken';
 
-describe("Token", () => { 
-    it("should generate token", () => { 
-       const username = "John"
-        const token = new Token();
+describe('Token', () => {
+  it('should generate token', () => {
+    const username = 'John';
+    const token = new Token();
 
-        token.generateToken(username);
-        const stringToken = token.getToken();
+    token.generateToken(username);
+    const stringToken = token.getToken();
 
-        expect(stringToken).toBeTypeOf('string');
-        expect(stringToken).toBeDefined();
-        
-    })
+    expect(stringToken).toBeTypeOf('string');
+    expect(stringToken).toBeDefined();
+  });
 
-    it("shuld return username from token", ()=> { 
-       const username = "John"
-        const token = new Token();
+  it('shuld return username from token', () => {
+    const username = 'John';
+    const token = new Token();
 
-        token.generateToken(username);
-        const stringToken = token.getToken();
-        const usernameFromToken = token.getUser(stringToken);
+    token.generateToken(username);
+    const stringToken = token.getToken();
+    const usernameFromToken = token.getUser(stringToken);
 
-        expect(usernameFromToken).toBe(username)
-    })
-})
-
-
+    expect(usernameFromToken).toBe(username);
+  });
+});
--- domain/token/token.ts
+++ domain/token/token.ts
@@ -1,52 +1,46 @@
-import jwt,{ Jwt, JwtPayload, decode} from "jsonwebtoken";
+import jwt, { Jwt, JwtPayload, decode } from 'jsonwebtoken';
 
-interface UsernameToken extends JwtPayload{ 
-username: string
+interface UsernameToken extends JwtPayload {
+  username: string;
 }
 
-const SECRET_KEY = "SUPER_SECRET_KEY"
+const SECRET_KEY = 'SUPER_SECRET_KEY';
 
-function decoder(err:Error| null, decode:UsernameToken){ 
-    if(err){ 
-        console.log("Error on parsing");
-        throw new Error("Error on decripting")
-    } 
-        return decode as UsernameToken
-} 
-
-
+function decoder(err: Error | null, decode: UsernameToken) {
+  if (err) {
+    console.log('Error on parsing');
+    throw new Error('Error on decripting');
+  }
+  return decode as UsernameToken;
+}
 
 class Token {
- token: string
+  token: string;
 
-  generateToken(username:string) {
+  generateToken(username: string) {
     this.token = jwt.sign({ username }, SECRET_KEY, {
-      expiresIn: "30min",
+      expiresIn: '30min'
     });
   }
   getUser(token: string) {
-        const tokenObject = jwt.verify(token, SECRET_KEY , decoder);
-        if(!tokenObject) throw new Error("Error on get user from token")
-        return tokenObject!.username;
+    const tokenObject = jwt.verify(token, SECRET_KEY, decoder);
+    if (!tokenObject) throw new Error('Error on get user from token');
+    return tokenObject!.username;
   }
-
 
   getToken() {
     return this.token;
   }
   verify(tokenToVerify, username) {
     try {
-      console.log("verify...");
-      const { username: usernameToken, exp: expirationTime } = jwt.verify(
-        tokenToVerify,
-        SECRET_KEY
-      );
+      console.log('verify...');
+      const { username: usernameToken, exp: expirationTime } = jwt.verify(tokenToVerify, SECRET_KEY);
 
       if (expirationTime > 0 && username == usernameToken) return true;
 
       return false;
     } catch (error) {
-      console.log("error", error);
+      console.log('error', error);
       return false;
     }
   }
