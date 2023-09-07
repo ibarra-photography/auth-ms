eb5712dc5f237ea13e60c13a11e93437e80c928f
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-09-07 14:30:25 +0200

    TEST(token) - should generate token && shuld return username from token

---
+++ domain/token/token.test.ts
@@ -0,0 +1,31 @@
+import { describe, it, expect } from 'vitest'
+
+import Token from "./token"
+import { Jwt } from 'jsonwebtoken'
+
+describe("Token", () => { 
+    it("should generate token", () => { 
+       const username = "John"
+        const token = new Token();
+
+        token.generateToken(username);
+        const stringToken = token.getToken();
+
+        expect(stringToken).toBeTypeOf('string');
+        expect(stringToken).toBeDefined();
+        
+    })
+
+    it("shuld return username from token", ()=> { 
+       const username = "John"
+        const token = new Token();
+
+        token.generateToken(username);
+        const stringToken = token.getToken();
+        const usernameFromToken = token.getUser(stringToken);
+
+        expect(usernameFromToken).toBe(username)
+    })
+})
+
+
