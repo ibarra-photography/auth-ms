88dd01e1038f02d46ddd6633b606c6d04f364c61
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-09-11 07:45:31 +0200

    ADD(Login) - Token on succesful login

--- router/controllers/loginController.ts
+++ router/controllers/loginController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { LoginViewModel } from '../../service/viewModels/loginViewModel';
 import { findUser } from '../../service/dataBaseConnection/findUser';
 import { textToHash } from '../../application/passwordEncriptation/textToHash';
+import Token from '../../domain/token/token'
 
 export const loginController = async (req: Request, res: Response) => {
   try {
@@ -10,10 +11,14 @@ export const loginController = async (req: Request, res: Response) => {
     const userViewModel = await findUser(loginUser.username);
     const hashPassword = await textToHash(loginUser.password);
     if (userViewModel!.password! !== hashPassword) throw new Error('Invalid username or password');
+    const token = new Token();
+    token.generateToken(loginUser.username);
+    const tokenFromUser = token.getToken();
+    
 
-    res.status(200).json({ token: '', message: 'Success!' });
+    res.status(200).json({ token: tokenFromUser, message: 'Success!' });
   } catch (error: Error | unknown) {
     console.log(error);
-    res.status(500).json({ message: error ? error : 'Error on login' });
+    res.status(500).json({ message: 'Error on login' });
   }
 };
