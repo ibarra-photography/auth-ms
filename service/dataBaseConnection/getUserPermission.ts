5e260c2d0c77ef64585452e52ed53b2b28ca3989
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-09-14 07:52:02 +0200

    ADD - get user permisions from db

---
+++ service/dataBaseConnection/getUserPermission.ts
@@ -0,0 +1,31 @@
+import { Document, WithId } from 'mongodb';
+import { findUserMaper } from '../../application/mapers/findUserMapper';
+import { User } from '../../domain/user';
+import { LoginViewModel } from '../viewModels/loginViewModel';
+import { usersCollection, mongoClient, getPointer } from './mongoClient';
+
+interface UserDocument extends Document, User {}
+
+export async function getUserPermissions(username: string) {
+  try {
+    await mongoClient.connect();
+    const userPointer = getPointer(usersCollection);
+
+    let user: User;
+    const userDocument: WithId<UserDocument> | null = await userPointer.findOne({ username: username });
+    if (!userDocument) throw new Error('Invalid username or passwod');
+    else if (userDocument) {
+      user = { ...userDocument };
+      const userViewModel: LoginViewModel = findUserMaper(user);
+      return userViewModel;
+    }
+  } catch (error: Error | unknown) {
+    console.log(Error.toString());
+    throw new Error(Error.toString());
+  } finally {
+    setTimeout(async () => {
+      await mongoClient.close();
+      console.log('Db closed');
+    }, 4000);
+  }
+}
