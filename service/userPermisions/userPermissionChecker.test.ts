341cb57ec06b4d5efd26dc527ead919bec19b07c
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-09-15 06:29:35 +0200

    FORMAT

--- service/userPermisions/userPermissionChecker.test.ts
+++ service/userPermisions/userPermissionChecker.test.ts
@@ -1,18 +1,13 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect } from 'vitest';
 import { userPermissionChecker, Permissions } from './userPermissionChecker';
 
-describe("User permissions checker", ()=> { 
-it("should have admin permissions", async ()=> {  
-    const usernmae = "admin";
-        const permissions : Permissions[]= ['admin']
+describe('User permissions checker', () => {
+  it('should have admin permissions', async () => {
+    const usernmae = 'admin';
+    const permissions: Permissions[] = ['admin'];
 
+    const hasPermissions = await userPermissionChecker(usernmae, permissions);
 
-        const hasPermissions = await userPermissionChecker(usernmae, permissions);
-
-
-        expect(hasPermissions).toBe(true);
-
-
-
-    })
-})
+    expect(hasPermissions).toBe(true);
+  });
+});
--- service/userPermisions/userPermissionChecker.ts
+++ service/userPermisions/userPermissionChecker.ts
@@ -1,17 +1,16 @@
-import {getUserPermissions} from '../../service/dataBaseConnection/getUserPermission'
+import { getUserPermissions } from '../../service/dataBaseConnection/getUserPermission';
 
 export type Permissions = 'invite' | 'post' | 'admin';
 
-export async function  userPermissionChecker(username: string, permissions: Permissions[]) {
-    
-    const permissionsFromDB = await getUserPermissions(username);
-    console.log("Permissions: ", permissionsFromDB)
-    if(!permissionsFromDB) return false
+export async function userPermissionChecker(username: string, permissions: Permissions[]) {
+  const permissionsFromDB = await getUserPermissions(username);
+  console.log('Permissions: ', permissionsFromDB);
+  if (!permissionsFromDB) return false;
 
-    let hasPermission = true;
-    permissions.forEach(permission => {
-        console.log(permission)
-        if (permissionsFromDB[permission] === false) hasPermission = false;
-    });
-    return hasPermission;
+  let hasPermission = true;
+  permissions.forEach(permission => {
+    console.log(permission);
+    if (permissionsFromDB[permission] === false) hasPermission = false;
+  });
+  return hasPermission;
 }
