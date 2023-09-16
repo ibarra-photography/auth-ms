a1c95a29c3ab82e7e73f4d950e2757bc4a673f2e
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-09-16 17:47:58 +0200

    ADD(GenerateInvitationController) - Permission validation

--- router/controllers/generateInvitationController.ts
+++ router/controllers/generateInvitationController.ts
@@ -1,15 +1,21 @@
 import { Request, Response } from 'express';
+
 import { ValidateUserViewModel } from 'service/viewModels/validateUserViewModel';
 import { userValidationService } from '../../service/validation/userValidationService';
+import { getUserPermissions } from "../../service/dataBaseConnection/getUserPermission";
+import { UserPermissionsViewModel } from '../../service/viewModels/userPermissionsViewModel';
 
-export const generateInvitationController = (req: Request, res: Response) => {
+export const generateInvitationController = async (req: Request, res: Response) => {
   const invitationRequest: ValidateUserViewModel = req.body;
-  console.log(invitationRequest);
-  // Validate user
+
   const isTokenValid = userValidationService(invitationRequest.username, invitationRequest.token);
   if (!isTokenValid) res.status(401).json({ message: 'Unauthorithed' });
 
-  // Check permissions
+    const permissions:UserPermissionsViewModel | undefined = await getUserPermissions(invitationRequest.username);
+   if( !permissions || !permissions!.invite) { 
+        console.log(`${invitationRequest.username} has no permission`)
+        res.status(403).json({message: "Forbiden"});
+    }
   // Generate invitation
 
   try {
