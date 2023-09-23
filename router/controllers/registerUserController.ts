89e25d6bbb23ff87e88e96fb6ab573bea18774f9
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-09-23 07:24:30 +0200

    ADD - invitation validation and inv deactivation

--- router/controllers/registerUserController.ts
+++ router/controllers/registerUserController.ts
@@ -3,14 +3,24 @@ import { RegisterUserViewModel } from '../../service/viewModels/registerUserView
 
 import { insetUser } from '../../service/dataBaseConnection/insertUser';
 import { textToHash } from '../../application/passwordEncriptation/textToHash';
+import { invitationsValidator } from '../../application/invitations/invitationsValidator';
+import { markInvitationAsUsed } from '../../service/dataBaseConnection/markInvitationAsUsed';
 
 export const registerUserController = async (req: Request, res: Response) => {
   try {
     const userViewModel: RegisterUserViewModel = req.body;
+    const isInvitationValid = await invitationsValidator(userViewModel.invitation);
+    if (!isInvitationValid) {
+      res.status(409).json({ message: 'invalid invitation' });
+      return;
+    }
     userViewModel.password = await textToHash(userViewModel.password);
     await insetUser(userViewModel);
+    await markInvitationAsUsed(userViewModel.invitation);
     res.status(202).json({ message: 'User added' });
   } catch (error: Error | unknown) {
+    // restore user and invitation
+    console.warn(Error.name);
     res.status(500).json({ message: 'Error adding user' });
   }
 };
